26d221691fe9d13034ffbdbde1f597f6889f9f89
Nazmus Sakib (Hulu) <54477339+ioryskb@users.noreply.github.com>
2026-08-09 01:35:46 +0600

    A temperature converter app for console.
    This is the roadmap.sh temperature converter app.

---
+++ README.txt
@@ -0,0 +1,3 @@
+1. According to the challenge the 3 functions are created.
+2. The values added in the Fahrenheit, Celsius, freezingFarenheit, and freezingCelsius will be converted.
+3. All the values are added in the console.log.
---
+++ script.js
@@ -0,0 +1,20 @@
+const fahrenheit = celsiusToFahrenheit(25)
+const celsius = fahrenheitToCelsius(68)
+const freezingFahrenheit = celsiusToFahrenheit(0);
+const freezingCelsius = fahrenheitToCelsius(32);
+
+function celsiusToFahrenheit(celsius) {
+    return (celsius * 1.8) + 32
+}
+function fahrenheitToCelsius(fahrenheit) {
+    return (fahrenheit - 32) * (5/9)
+}
+
+function formatTemperature(value, unit){
+        return `${value} ${unit}`
+}
+
+console.log(formatTemperature(fahrenheit, 'F'));
+console.log(formatTemperature(celsius, 'C'));
+console.log(formatTemperature(freezingFahrenheit, 'F'));
+console.log(formatTemperature(freezingCelsius, 'C'));
